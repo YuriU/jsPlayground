9503b38466a933f2af9475333bd6a98a9523733f
YuriU <jerryhire8@gmail.com>
2020-09-21 19:23:43 +0300

    Added initial version

---
+++ ElectronFromScratch/03_BugLogger/src/components/App.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
+import LogItem from './LogItem';
+import AddLogItem from './AddLogItem';
+
+const App = () => {
+
+	const [logs, setLogs] = useState([
+		{
+			_id: 1,
+			text: 'This is log one',
+			priority: 'low',
+			user: 'Brad',
+			created: new Date().toString(),
+		},
+		{
+			_id: 2,
+			text: 'This is log two',
+			priority: 'moderate',
+			user: 'Kate',
+			created: new Date().toString(),
+		},
+		{
+			_id: 3,
+			text: 'This is log three',
+			priority: 'high',
+			user: 'John',
+			created: new Date().toString(),
+		},
+	]);
+
+	function addItem(item) {
+		item._id = Math.floor(Math.random()*90000)+1000;
+		item.created = new Date().toString();
+
+		setLogs([...logs, item]);
+	}
+
+	return (
+		<Container className='app'>
+			<AddLogItem addItem={addItem}/>
+			<Table>
+				<thead>
+					<tr>
+						<th>Priority</th>
+						<th>Log Text</th>
+						<th>User</th>
+						<th>Created</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					{
+						logs.map((log) => 
+							(<LogItem key={log._id} log={log} />)
+						)
+					}
+				</tbody>
+			</Table>
+		</Container>
+	)
+}
+
+export default App
