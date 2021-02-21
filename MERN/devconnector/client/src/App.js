5dc29ae4a191bf2e29e753dae2f654490a2f0436
YuriU <jerryhire8@gmail.com>
2021-02-21 18:21:49 +0200

    Added load user functionality

--- MERN/devconnector/client/src/App.js
+++ MERN/devconnector/client/src/App.js
@@ -1,18 +1,28 @@
 import './App.css';
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Navbar } from './components/layout/Navbar';
 import { Landing } from './components/layout/Landing';
 import { Login } from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
+import setAuthToken from './utils/setAuthToken';
 
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
 
-const App = () => 
-  <Provider store={store}>
+if(localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+ return (<Provider store={store}>
     <Router>
       <Fragment>
         <Navbar />
@@ -26,7 +36,8 @@ const App = () =>
         </section>
       </Fragment>
     </Router>
-  </Provider>
+  </Provider>)
+}
   
 
 export default App;
--- MERN/devconnector/client/src/reducers/auth.js
+++ MERN/devconnector/client/src/reducers/auth.js
@@ -1,6 +1,8 @@
 import {
     REGISTER_SUCCESS,
-    REGISTER_FAIL
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR
 } from '../actions/types';
 
 const initialState = {
@@ -29,6 +31,13 @@ export default function(state = initialState, action) {
                 token: null,
                 isAuthenticated: false,
                 loading: false
+            }
+        case USER_LOADED:
+            return {
+                ...state,
+                isAuthenticated: true,
+                loading: false,
+                user: payload
             }
 
         default:
