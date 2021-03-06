e543fd0bf0c3536454d76d9b27fbecc2db824887
YuriU <jerryhire8@gmail.com>
2021-03-06 12:41:42 +0200

    Added like functionality

--- MERN/devconnector/client/src/components/posts/PostItem.js
+++ MERN/devconnector/client/src/components/posts/PostItem.js
@@ -3,8 +3,12 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
+import { addLike, removeLike } from '../../actions/post';
 
-const PostItem = ( { auth, post: { _id, text, name, avatar, user, likes, comments, date } } ) => {
+const PostItem = ( { auth, 
+                    post: { _id, text, name, avatar, user, likes, comments, date }, 
+                    addLike, 
+                    removeLike } ) => {
     return (
         <div className="post bg-white p-1 my-1">
           <div>
@@ -24,13 +28,13 @@ const PostItem = ( { auth, post: { _id, text, name, avatar, user, likes, comment
              <p className="post-date">
                 Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
             </p>
-            <button type="button" className="btn btn-light">
+            <button type="button" onClick={e => addLike(_id)} className="btn btn-light">
               <i className="fas fa-thumbs-up"></i>
               {likes.length > 0 && 
                 <span>{' '}{likes.length}</span>
               }
             </button>
-            <button type="button" className="btn btn-light">
+            <button type="button" onClick={e => removeLike(_id)} className="btn btn-light">
               <i className="fas fa-thumbs-down"></i>
             </button>
             {
@@ -59,4 +63,4 @@ const mapStateToProps = state => ({
     auth: state.auth
 });
 
-export default connect(mapStateToProps, {})(PostItem)
+export default connect(mapStateToProps, { addLike, removeLike })(PostItem)
