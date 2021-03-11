558519d9e31bedafa7d59d6640ac605d27069331
YuriU <jerryhire8@gmail.com>
2021-03-11 12:48:30 +0200

    Added Autocomplete

--- search-Index/src/app.js
+++ search-Index/src/app.js
@@ -11,11 +11,29 @@ buildIndexBtn.addEventListener('click', async () => {
     
 
     console.log(si);
-    const results = await si.QUERY('мир', { DOCUMENTS: true })
+    const results = await si.QUERY('file', { DOCUMENTS: true })
+
+    
     
     console.log(results);
+
+    const results2 = await si.DICTIONARY('file');
+    console.log(results2);
   }
 });
+
+// listen for typing and create suggestions. Listen for clicked
+// suggestions
+autocomplete('#query', { hint: false }, [{
+  source: (query, cb) => (query.length >= 3)
+    ? si.DICTIONARY(query).then(cb)
+    // eslint-disable-next-line
+    : cb([]),
+  templates: { suggestion: suggestion => suggestion }
+}]).on('autocomplete:selected', function (event, suggestion) {
+  //search(suggestion)
+  console.log(suggestion);
+})
 
 
 /* INITIALIZE */
@@ -24,22 +42,23 @@ Promise.all([
     name: 'mySearchIndex',
     stopwords: []
   }),
-  //fetch('generate-index/EarthPorn-top-search-index.json').then(res => res.json())
-]).then( async ([thisSi]) => {
+  fetch('data/en_US.json').then(res => res.json())
+]).then( async ([thisSi, data ]) => {
   // set global variable (in practice you might not want to do this)
   si = thisSi
 
+  //console.log(data)
+
   //await si.FLUSH()
-  await si.PUT([{
-    name: 'Hello',
-    value: 'World'
-  }])
+  //await si.PUT(data);
+  const converted = data.map(i => ({ 
+    title: i.Title,
+    description: i.Description ? i.Description : '',
+    body: i.Body ? i.Body : ''
+  }));
 
-  await si.PUT([{
-    name: 'Мир',
-    value: 'Труд'
-  }])
-
+  await si.PUT(converted)
+  
   // replicate pregenerated index
   //si.IMPORT(dump).then(search)
 }).catch(console.log)
